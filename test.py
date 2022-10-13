95817a104da664ff56d55ca26c341e05e3a75eb1
Tim Eric Stache <tist925c@tu-dresden.de>
2022-10-13 13:48:29 +0200

    files

---
+++ test.py
@@ -0,0 +1,51 @@
+"""Testing server.py"""
+from unittest import TestCase
+from unittest.mock import patch
+
+from server import Person, get_person, simulate_db_persons
+
+
+class TestServerFunctions(TestCase):
+    """testing something"""
+
+    def test_getting_the_person(self) -> None:
+        """gets a person"""
+
+        expected = [
+            Person(name="P1", age=1),
+            Person(name="P2", age=2),
+        ]
+
+        with patch("server.simulate_db_persons") as mock:
+            mock.return_value = expected
+
+            got = get_person()
+
+            self.assertListEqual(
+                expected,
+                got,
+            )
+
+
+class TestSimulateDBPersons(TestCase):
+    """Something"""
+
+    def test_read_persons_db(self) -> None:
+        """i dont want to write a dockstring now"""
+
+        persons = simulate_db_persons()
+        persons_str: list[str] = []
+        for person in persons:
+            persons_str.append(f"{person.name}, {person.age}\n")
+        with open("database", encoding="utf-8") as file:
+            self.assertListEqual(list(file), persons_str)
+
+
+class TestAddPerson(TestCase):
+    """add Person"""
+
+    def test_list_len(self) -> None:
+        self.assertEqual(
+            len(simulate_db_persons()),
+            len(addPerson(Person(name="Kurt", age=42))),
+        )

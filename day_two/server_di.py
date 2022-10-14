2e5b84f7b9db5abc3620aa94b240f0f26dedcad5
Leon Albert <leal255e@tu-dresden.de>
2022-10-14 11:52:56 +0200

    fixed:all

--- day_two/server_di.py
+++ day_two/server_di.py
@@ -1,14 +1,64 @@
-"""Wuff Wuff DOGString"""
+"""server with dependency injection"""
 # pylint: disable=disallowed-name
-# pylint: disable=unused-argument
-from typing import Callable, Optional
+
+from typing import Any, Callable, Iterator, Optional, Protocol, TypeVar
 
 from server import Person
 
+Self = TypeVar("Self")
 
-def _read_database_file():
-    with open("database", encoding="utf-8") as file:
-        return file.readlines()
+
+class PHandler(Protocol):
+    """Handler protocol"""
+
+    def __enter__(self: Self) -> Self:
+        ...
+
+    def __exit__(self, *args: Any, **kwargs: Any) -> None:
+        ...
+
+    def __iter__(self) -> Iterator[str]:
+        ...
+
+    def readlines(self) -> list[str]:
+        """read lines"""
+
+    def writelines(self, lines: list[str]) -> None:
+        """write lines"""
+
+
+def read_lines_factory(
+    open_file: Callable[[str, str], PHandler]
+) -> Callable[[], list[str]]:
+    """..."""
+
+    def run() -> list[str]:
+        with open_file("person_db", "r") as file:
+            return file.readlines()
+
+    return run
+
+
+def write_lines_factory(
+    open_file: Callable[[str, str], PHandler]
+) -> Callable[[list[str]], None]:
+    """..."""
+
+    def run(lines: list[str]) -> None:
+        with open_file("person_db", "w") as file:
+            file.writelines(lines)
+
+    return run
+
+
+def _read_from_db_file() -> list[str]:
+    with open("person_db", encoding="utf-8") as file:
+        return list(file)
+
+
+def _write_to_db_file(lines: list[str]) -> None:
+    with open("person_db", "w", encoding="utf-8") as file:
+        file.writelines(lines)
 
 
 def simulate_db_person_factory(
@@ -50,16 +100,25 @@ def change_age_factory(
     return run
 
 
-simulate_database_person = simulate_db_person_factory(_read_database_file)
+# read_lines = read_lines_factory(open)
+# write_lines = write_lines_factory(open)
+# simulate_db_person = simulate_db_person_factory(read_lines)
+# change_name = change_name_factory(read_lines, write_lines)
+# change_age = change_name_factory(read_lines, write_lines)
+
+simulate_db_person = simulate_db_person_factory(_read_from_db_file)
+change_name = change_name_factory(_read_from_db_file, _write_to_db_file)
+change_age = change_name_factory(_read_from_db_file, _write_to_db_file)
 
 
 def _foo(bar: str) -> float:
-    """no"""
-    return float(bar)
+    return float(bar) ** 10
 
 
-def use_foo_factory(foo: Callable) -> Callable[[list[str]], list[float]]:
-    """ja"""
+def use_foo_factory(
+    foo: Callable[[str], float]
+) -> Callable[[list[str]], list[float]]:
+    """..."""
 
     def _real_foo(bars: list[str]):
         return [foo(b) for b in bars]
--- day_two/test_server_di.py
+++ day_two/test_server_di.py
@@ -1,27 +1,158 @@
-"""testing"""
-# pylint: disable=disallowed-name
-# pylint: disable=unused-argument
-from unittest import TestCase
-from urllib.parse import non_hierarchical
+"""test server_di"""
 
-from server_di import change_name_factory, simulate_db_person_factory, use_foo_factory
+
+from unittest import TestCase
+from unittest.mock import Mock, call
+
 from server import Person
+from server_di import (
+    change_name_factory,
+    simulate_db_person_factory,
+    use_foo_factory,
+)
+
+
+class TestSimulateDBPersonFactory(TestCase):
+    """test simulate_db_person_factory"""
+
+    def test_get_only_person(self) -> None:
+        """..."""
+
+        def get_lines() -> list[str]:
+            return ["Marry, 25\n"]
+
+        get_persons = simulate_db_person_factory(get_lines)
+        self.assertListEqual(
+            [Person(name="Marry", age=25)],
+            get_persons(),
+        )
+
+    def test_ignore_too_few_item_lines_persons(self) -> None:
+        """..."""
+
+        def get_lines() -> list[str]:
+            return [
+                "Marry, 25\n",
+                "Harry\n",
+                "Sebastian, 77\n",
+            ]
+
+        get_persons = simulate_db_person_factory(get_lines)
+        self.assertListEqual(
+            [
+                Person(name="Marry", age=25),
+                Person(name="Sebastian", age=77),
+            ],
+            get_persons(),
+        )
+
+    def test_ignore_additional_line_items_persons(self) -> None:
+        """..."""
+
+        def get_lines() -> list[str]:
+            return [
+                "Marry, 25\n",
+                "Harry, 55\n",
+                "Sebastian, 77, 66\n",
+            ]
+
+        get_persons = simulate_db_person_factory(get_lines)
+        self.assertListEqual(
+            [
+                Person(name="Marry", age=25),
+                Person(name="Harry", age=55),
+                Person(name="Sebastian", age=77),
+            ],
+            get_persons(),
+        )
+
+    def test_ignore_invalid_age(self) -> None:
+        """..."""
+
+        def get_lines() -> list[str]:
+            return [
+                "Marry, zz\n",
+                "Harry, 55\n",
+                "Sebastian, 77, 66\n",
+            ]
+
+        get_persons = simulate_db_person_factory(get_lines)
+        self.assertListEqual(
+            [
+                Person(name="Harry", age=55),
+                Person(name="Sebastian", age=77),
+            ],
+            get_persons(),
+        )
+
+    def test_get_some_persons(self) -> None:
+        """..."""
+
+        def get_lines() -> list[str]:
+            return [
+                "Marry, 25\n",
+                "Harry, 105\n",
+                "Sebastian, 77\n",
+            ]
+
+        get_persons = simulate_db_person_factory(get_lines)
+        self.assertListEqual(
+            [
+                Person(name="Marry", age=25),
+                Person(name="Harry", age=105),
+                Person(name="Sebastian", age=77),
+            ],
+            get_persons(),
+        )
+
+
+class TestChangeNameFactory(TestCase):
+    """test change_name_factory"""
+
+    def test_change_name(self) -> None:
+        """..."""
+        written_lines: list[str] = []
+
+        def get_lines() -> list[str]:
+            return ["Marry, 33\n"]
+
+        def write_lines(lines: list[str]) -> None:
+            nonlocal written_lines
+            written_lines = lines
+
+        change_name = change_name_factory(get_lines, write_lines)
+        change_name("Marry", "Sue")
+
+        self.assertListEqual(["Sue, 33\n"], written_lines)
+
+    def test_change_name_with_mock(self) -> None:
+        """..."""
+
+        def get_lines() -> list[str]:
+            return ["Marry, 33\n"]
+
+        write_lines = Mock()
+
+        change_name = change_name_factory(get_lines, write_lines)
+        change_name("Marry", "Sue")
+
+        write_lines.assert_called_once_with(["Sue, 33\n"])
 
 
 class TestUseFoo(TestCase):
-    """nö"""
+    """test use_foo"""
 
     def test_converts(self) -> None:
-        """ldsjf!!!?"""
+        """..."""
 
-        def mock_foo(bar: str) -> float:
+        def mock_foo(_: str) -> float:
             return 42
 
         use_foo = use_foo_factory(mock_foo)
         self.assertEqual([42, 42], use_foo(["", ""]))
 
-    def test_converts_corrects_bar(self) -> None:
-        """dhfskhsda7856757,.-,.#+,.-ß,.-"""
+    def test_use_correct_item_bar(self) -> None:
+        """..."""
 
         def mock_foo(value: str) -> float:
             return float(value)
@@ -29,58 +160,23 @@ class TestUseFoo(TestCase):
         use_foo = use_foo_factory(mock_foo)
         self.assertEqual([4, 2], use_foo(["4", "2"]))
 
-class TestSimulateDBPersonFactory(TestCase):
-    """42"""
+    def test_use_correct_item_via_list(self) -> None:
+        """..."""
+        calls: list[str] = []
 
-    def test_get_only_person(self) -> None:
-        """42"""
+        def mock_foo(value: str) -> float:
+            calls.append(value)
+            return 42
 
-        def get_lines() -> list[str]:
-            return ["Marry, 19\n"]
+        use_foo = use_foo_factory(mock_foo)
+        _ = use_foo(["4", "2"])
+        self.assertEqual(["4", "2"], calls)
 
-        get_persons = simulate_db_person_factory(get_lines)
-        self.assertEqual(
-            [Person(name="Marry", age="19")],
-            get_persons()
-        )
-
-    def test_ignore_error_lines(self) -> None:
-        """42"""
-
-        def get_lines() -> list[str]:
-            return [
-                "Marry, 25\n",
-                "Harry, 55\n",
-                "Sebastian, 177, 56\n"
-            ]
-
-        get_persons = simulate_db_person_factory(get_lines)
-        self.assertEqual(
-            [
-                Person(name="Marry", age="25"),
-                Person(name="Harry", age="55"),
-                Person(name="Sebastian", age="77"),
-            ],
-            get_persons()
-        )
-
-
-class TestChangeNameFactory(TestCase):
-    """..."""
-
-    def test_change_name(self) -> None:
+    def test_use_correct_item_via_mock(self) -> None:
         """..."""
 
+        mock_foo = Mock(return_value=42)
 
-        written_lines: list[str] = []
-        def get_lines() -> list[str]:
-            return ["Marry, 33"]
-
-        def write_lines(line: list[str]) -> None:
-            nonlocal write_lines
-            write_lines = lines
-
-        change_name = change_name_factory(get_lines, write_lines)
-        change_name("Marry", "Sue")
-
-        self.assertListEqual(["Sue, 33\n"], written_lines)
+        use_foo = use_foo_factory(mock_foo)
+        _ = use_foo(["4", "2"])
+        mock_foo.assert_has_calls([call("4"), call("2")])
